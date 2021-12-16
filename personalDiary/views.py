0c0bbbab676bffc3b09bdef14323977a88a75299
Abdullah Awad <abdullah.alashafi@gmail.com>
2021-12-16 22:03:39 +0300

    login decorator was added to class view

--- personalDiary/views.py
+++ personalDiary/views.py
@@ -6,7 +6,8 @@ from django.views.generic import DetailView, UpdateView, DeleteView, ListView, C
 from .forms import DiaryForm
 from .models import Diary
 from django.contrib.auth.models import User
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 APP_NAME = "personalDiary"
 def set_template_name(template_name):
@@ -15,6 +16,13 @@ def set_template_name(template_name):
 
 class ListDiaryView(ListView):
     context_object_name='diaries'
+
+        
+    #* must user login #* must user login 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get_queryset(self):
         user = User.objects.get(username=self.request.user.username)
         # print(type(user))
@@ -26,6 +34,12 @@ class CreateDiaryView(CreateView):
     fields=['title', 'body']
     template_name='personalDiary/new_diary.html'
 
+
+    #* must user login 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def form_valid(self, form):
         form.instance.user=self.request.user
         return super().form_valid(form)
@@ -34,13 +48,28 @@ class CreateDiaryView(CreateView):
 class DetailDiaryView(DetailView):
     model=Diary
 
+    #* must user login 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
 
 class UpdateDiaryView(UpdateView):
     model=Diary
     fields=['title', 'body']
     # success_url=reverse_lazy('detail-diary')
 
+    #* must user login 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
 class DeleteDairyView(DeleteView):
     model=Diary
     success_url=reverse_lazy('personalDiary:home')
+
+    #* must user login 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
     
