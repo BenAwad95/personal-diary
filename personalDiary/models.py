837658ef9f72d917db5c8c4bb7c59fa40b35fe1c
Eng, Abdullah <abdullah.alashafi@gmail.com>
2022-12-23 23:05:11 +0300

    create a new mood field

--- personalDiary/models.py
+++ personalDiary/models.py
@@ -3,9 +3,18 @@ from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.contrib.auth.models import User
 class Diary(models.Model):
+    NORMAL = 'n'
+    HAPPY = 'h'
+    SAD = 's'
+    MOOD_CHOICES = [
+        (NORMAL, 'Normal'),
+        (HAPPY, 'Happy'),
+        (SAD, 'Sad')
+    ]
     user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE, null=True)
     title = models.CharField(_("title"), max_length=50)
     body = models.TextField(_("diary body"))
+    mood = models.CharField(_("mood"), max_length=50, choices=MOOD_CHOICES, default=NORMAL)
     created = models.DateTimeField(_("diary created date"), auto_now_add=True)
     updated = models.DateTimeField(_("diary updated date"), auto_now=True)
 
